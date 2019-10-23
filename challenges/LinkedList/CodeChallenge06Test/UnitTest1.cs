23bc6f0d7682b07137fd77a536520626905ce9f8
Kyungrae <jeremiah.kim@hotmail.com>
2019-10-22 17:29:35 -0700

    scaffolding for the code challenge 06

---
+++ challenges/LinkedList/CodeChallenge06Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using static LinkedList.Program;
+
+namespace CodeChallenge06Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ challenges/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
