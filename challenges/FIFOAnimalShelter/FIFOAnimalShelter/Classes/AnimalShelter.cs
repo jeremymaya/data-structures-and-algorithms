e41f07cf203f54fa03053714c90a80a5d562e712
Kyungrae <jeremiah.kim@hotmail.com>
2019-10-29 14:09:02 -0700

    Enqueue(animal) method completed

---
+++ challenges/FIFOAnimalShelter/FIFOAnimalShelter/Classes/AnimalShelter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace FIFOAnimalShelter.Classes
+{
+    public class AnimalShelter
+    {
+        public Stack stackOne = new Stack();
+        public Stack stackTwo = new Stack();
+
+        public void Enqueue(object animal)
+        {
+            while (stackOne.Count != 0)
+            {
+                stackTwo.Push(stackOne.Pop());
+            }
+
+            stackOne.Push(animal);
+
+            while (stackTwo.Count != 0)
+            {
+                stackOne.Push(stackTwo.Pop());
+            }
+        }
+    }
+}
