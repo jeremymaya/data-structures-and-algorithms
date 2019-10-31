3358a73233c6ee7433fd33e1c4fbe6ea81a998cf
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-10-31 12:40:58 -0700

    scaffolding for Code Challenge 13

---
+++ challenges/MultiBracketValidation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiBracketValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
