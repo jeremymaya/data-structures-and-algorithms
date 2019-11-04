40db041553c84e1e7a9ab18d47f695abe5213187
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-11-03 20:40:28 -0800

    Stack class and Push method completed

--- Data-Structures/StacksAndQueues/CodeChallenge10Test/UnitTest1.cs
+++ Data-Structures/StacksAndQueues/CodeChallenge10Test/UnitTest1.cs
@@ -1,14 +1,30 @@
 using System;
 using Xunit;
+using StacksAndQueues.Classes;
 
 namespace CodeChallenge10Test
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CanSuccessfullyPushOntoAStack()
         {
+            Node testNode = new Node(1);
+            Stack testStack = new Stack();
+            testStack.Push(1);
 
+            Assert.Equal(1, testStack.Top.Value);
+        }
+
+        [Fact]
+        public void CanSuccessfullyPushMultipleValuesOntoAStack()
+        {
+            Node testNode = new Node(1);
+            Stack testStack = new Stack();
+            testStack.Push(1);
+            testStack.Push(2);
+
+            Assert.Equal(2, testStack.Top.Value);
         }
     }
 }
---
+++ Data-Structures/StacksAndQueues/StacksAndQueues/Classes/Stack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    public class Stack
+    {
+        public Node Top { get; set; }
+
+        public void Push(int value)
+        {
+            if(Top == null)
+            {
+                Top = new Node(value);
+            }
+            Node newTop = new Node(value);
+            Top.Next = newTop;
+            Top = Top.Next;
+        }
+    }
+}
