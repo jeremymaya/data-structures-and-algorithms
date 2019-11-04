91f8382acd6d291dad9dbfcc8021b85d480a94a5
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-11-03 18:35:24 -0800

    Node class completed

---
+++ Data-Structures/StacksAndQueues/StacksAndQueues/Classes/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueues.Classes
+{
+    public class Node
+    {
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+}
