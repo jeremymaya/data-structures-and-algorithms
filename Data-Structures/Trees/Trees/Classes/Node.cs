3d8f7889241faed221d4dc7f9bae93a41015894b
Kyungrae <jeremiah.kim@hotmail.com>
2019-11-07 12:33:26 -0800

    Node class completed

--- Data-Structures/Trees/Trees/Classes/Node.cs
+++ Data-Structures/Trees/Trees/Classes/Node.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 namespace Trees.Classes
 {
-    public class Node
+    public class Node<T>
     {
-        public Node()
+        public T Value { get; set; }
+        public Node<T> LeftChild { get; set; }
+        public Node<T> RightChild { get; set; }
+
+        public Node(T value)
         {
+            Value = value;
         }
     }
 }
