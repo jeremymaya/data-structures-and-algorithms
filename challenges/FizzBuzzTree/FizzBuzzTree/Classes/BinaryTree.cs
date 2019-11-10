246c3efc7d281346654511152dd61e1e11bcaa2b
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-11-09 23:16:54 -0800

    FizzBuzz method and helper method completed

---
+++ challenges/FizzBuzzTree/FizzBuzzTree/Classes/BinaryTree.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzzTree.Classes
+{
+    public class BinaryTree<T>
+    {
+        public Node<T> Root { get; set; }
+
+        /// <summary>
+        /// Constructor method that creates a BinaryTree object with a root node
+        /// </summary>
+        /// <param name="value"></param>
+        public BinaryTree(T value)
+        {
+            Root = new Node<T>(value);
+        }
+
+        /// <summary>
+        /// Contructor method that creates an empty BinarySearchTree object
+        /// </summary>
+        public BinaryTree()
+        {
+
+        }
+
+        /// <summary>
+        /// A non-resursive method that creates and return a generic list to store nodes from resursive helper method PreOrder
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public List<T> PreOrder(Node<T> node)
+        {
+            List<T> traversal = new List<T>();
+
+            PreOrder(node, traversal);
+
+            return traversal;
+        }
+
+        /// <summary>
+        /// A recursive helper method that:
+        /// 1. Adds Root node to the call stack then adds to a generic list
+        /// 2. Adds LeftChild to the call stack then adds to the generic list until there is no more LeftChild node
+        /// 3. Adds RightChild to the call stack then adds to the generic list until there is no more RightChild node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="traversal"></param>
+        private void PreOrder(Node<T> node, List<T> traversal)
+        {
+            traversal.Add(node.Value);
+
+            if(node.LeftChild != null)
+            {
+                PreOrder(node.LeftChild, traversal);
+            }
+            if(node.RightChild != null)
+            {
+                PreOrder(node.RightChild, traversal);
+            }
+        }
+
+        /// <summary>
+        /// A non-resursive method that creates and return a generic list to store nodes from resursive helper method InOrder
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public List<T> InOrder(Node<T> node)
+        {
+            List<T> traversal = new List<T>();
+
+            InOrder(node, traversal);
+
+            return traversal;
+        }
+
+        /// <summary>
+        /// A recursive helper method that:
+        /// 1. Adds LeftChild to the call stack then adds to the generic list until there is no more LeftChild node 
+        /// 2. Adds Root node to the call stack then adds to a generic list
+        /// 3. Adds RightChild to the call stack then adds to the generic list until there is no more RightChild node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="traversal"></param>
+        private void InOrder(Node<T> node, List<T> traversal)
+        {
+            if(node.LeftChild != null)
+            {
+                InOrder(node.LeftChild, traversal);
+            }
+
+            traversal.Add(node.Value);
+
+            if (node.RightChild != null)
+            {
+                InOrder(node.RightChild, traversal);
+            }
+        }
+
+        /// <summary>
+        /// A non-resursive method that creates and return a generic list to store nodes from resursive helper method PostOrder
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public List<T> PostOrder(Node<T> node)
+        {
+            List<T> traversal = new List<T>();
+
+            PostOrder(node, traversal);
+
+            return traversal;
+        }
+
+        /// <summary>
+        /// A recursive helper method that:
+        /// 1. Adds LeftChild to the call stack then adds to the generic list until there is no more LeftChild node 
+        /// 2. Adds RightChild to the call stack then adds to the generic list until there is no more RightChild node
+        /// 3. Adds Root node to the call stack then adds to a generic list
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="traversal"></param>
+        private void PostOrder(Node<T> node, List<T> traversal)
+        {
+            if (node.LeftChild != null)
+            {
+                PreOrder(node.LeftChild, traversal);
+            }
+            if (node.RightChild != null)
+            {
+                PreOrder(node.RightChild, traversal);
+            }
+
+            traversal.Add(node.Value);
+        }
+
+        /// <summary>
+        /// Method that traveres a tree using a queue and while loop
+        /// While there is no more child nodes, as the enqued node deques, it enques LeftChild and RightChild nodes
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public List<T> BreadthFirst(Node<T> node)
+        {
+            Queue<Node<T>> breadth = new Queue<Node<T>>();
+            List<T> values = new List<T>();
+
+            breadth.Enqueue(node);
+
+            while(breadth.TryPeek(out node))
+            {
+                Node<T> front = breadth.Dequeue();
+                values.Add(front.Value);
+
+                if (front.LeftChild != null)
+                {
+                    breadth.Enqueue(front.LeftChild);
+                }
+
+                if (front.RightChild != null)
+                {
+                    breadth.Enqueue(front.RightChild);
+                }
+            }
+            return values;
+        }
+    }
+}
--- challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
+++ challenges/FizzBuzzTree/FizzBuzzTree/Program.cs
@@ -1,12 +1,56 @@
-﻿using System;
+﻿using FizzBuzzTree.Classes;
+using System;
 
 namespace FizzBuzzTree
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BinaryTree<object> tree = new BinaryTree<object>(1);
+
+            tree.Root.LeftChild = new Node<object>(2);
+            tree.Root.LeftChild.LeftChild = new Node<object>(4);
+            tree.Root.LeftChild.RightChild = new Node<object>(5);
+
+            tree.Root.RightChild = new Node<object>(3);
+            tree.Root.RightChild.LeftChild = new Node<object>(6);
+            tree.Root.RightChild.RightChild = new Node<object>(7);
+
+            Console.WriteLine(String.Join(", ", tree.BreadthFirst(tree.Root)));
+
+            BinaryTree<object> treeNew = FizzBuzz(tree);
+
+            Console.WriteLine(String.Join(", ", treeNew.BreadthFirst(treeNew.Root)));
+
+
+        }
+
+        static BinaryTree<object> FizzBuzz(BinaryTree<object> tree)
+        {
+            Node<object> node = tree.Root;
+
+            FizzBuzz(tree, node);
+
+            return tree;
+        }
+
+        private static void FizzBuzz(BinaryTree<object> tree, Node<object> node)
+        {
+            if (Convert.ToInt32(node.Value) % 3 == 0)
+            {
+                node.Value = "Fizz";
+            }
+
+            if (node.LeftChild != null)
+            {
+                FizzBuzz(tree, node.LeftChild);
+            }
+
+            if (node.RightChild != null)
+            {
+                FizzBuzz(tree, node.RightChild);
+            }
         }
     }
 }
