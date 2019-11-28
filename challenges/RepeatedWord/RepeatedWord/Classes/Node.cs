4818e6f07d51ff76a20a5d1aa673b3a7499891db
Kyungrae Kim <jeremiah.kim@hotmail.com>
2019-11-28 15:12:04 -0800

    challenge completed

---
+++ challenges/RepeatedWord/RepeatedWord/Classes/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepeatedWord.Classes
+{
+    public class Node
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
