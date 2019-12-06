3f756f3756da37723785a493f60453ecce5e8f8c
Kyungrae <jeremiah.kim@hotmail.com>
2019-12-06 09:11:52 -0800

    scaffolding for code challenge 38

---
+++ challenges/DepthFirst/DepthFirst/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DepthFirst
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
