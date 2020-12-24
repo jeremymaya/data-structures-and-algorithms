7f213d0d8cdc695d7ea71aa712087813ff572c39
Kyungrae Kim <kyungraekim@outlook.com>
2020-12-24 13:22:09 -0800

    folder location fixed

---
+++ data-structures/StackAndQueue/StackAndQueue/Stack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StackAndQueue
+{
+    public class Stack<T>
+    {
+        private readonly List<T> _stack;
+        public T Top { get; set; }
+
+
+        public Stack()
+        {
+            _stack = new List<T>();
+        }
+
+        public void Push(T value)
+        {
+            Top = value;
+
+            _stack.Add(value);
+        }
+
+        public T Pop()
+        {
+            int count = _stack.Count;
+
+            if (count == 0)
+                return default;
+
+            Top = count == 1 ? default : _stack[count - 1];
+
+            T pop = _stack[count - 1];
+            _stack.Remove(pop);
+
+            return pop;
+        }
+
+        public T Peek()
+        {
+            int count = _stack.Count;
+
+            if (count == 0)
+                return default;
+
+            return _stack[count - 1];
+        }
+
+        public bool IsEmpty()
+        {
+            if (_stack.Count == 0)
+                return true;
+
+            return false;
+        }
+    }
+}
---
+++ data-structures/StackAndQueue/XUnitTestSinglyLinkedList/UnitTestStack.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Xunit;
+using StackAndQueue;
+
+namespace XUnitTestSinglyLinkedList
+{
+    public class UnitTestStack
+    {
+        [Fact]
+        public void CanSuccessfullyInstantiateAnEmptyStack()
+        {
+            Stack<char> stack = new Stack<char>();
+
+            Assert.NotNull(stack);
+        }
+
+        [Fact]
+        public void CanSuccessfullyPushOntoAStack()
+        {
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push("candy cane");
+
+            Assert.NotNull(stack.Top);
+            Assert.Equal("candy cane", stack.Top);
+        }
+
+        [Theory]
+        [InlineData(new int[] { }, 0)]
+        [InlineData(new int[] { 1 }, 1)]
+        [InlineData(new int[] { 1, 2 }, 2)]
+        [InlineData(new int[] { 1, 2, 3 }, 3)]
+        public void CanSuccessfullyPushMultipleValuesOntoAStack(int[] nums, int expected)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            foreach (int num in nums)
+                stack.Push(num);
+
+            Assert.Equal(expected, stack.Top);
+        }
+
+        [Theory]
+        [InlineData(new int[] { }, 0)]
+        [InlineData(new int[] { 1 }, 1)]
+        [InlineData(new int[] { 1, 2 }, 2)]
+        [InlineData(new int[] { 1, 2, 3 }, 3)]
+        public void CanSuccessfullyPopOffTheStack(int[] nums, int expected)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            foreach (int num in nums)
+                stack.Push(num);
+
+            Assert.Equal(expected, stack.Pop());
+        }
+
+        [Theory]
+        [InlineData(new int[] { })]
+        [InlineData(new int[] { 1 })]
+        [InlineData(new int[] { 1, 2 })]
+        [InlineData(new int[] { 1, 2, 3 })]
+        public void CanSuccessfullyEmptyAStackAfterMultiplePops(int[] nums)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            foreach (int num in nums)
+                stack.Push(num);
+
+            while (!stack.IsEmpty())
+                stack.Pop();
+
+            Assert.Equal(default, stack.Peek());
+        }
+
+        [Theory]
+        [InlineData(new int[] { }, 0)]
+        [InlineData(new int[] { 1 }, 1)]
+        [InlineData(new int[] { 1, 2 }, 2)]
+        [InlineData(new int[] { 1, 2, 3 }, 3)]
+        public void CanSuccessfullyPeekTheNextItemOnTheStack(int[] nums, int expected)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            foreach (int num in nums)
+                stack.Push(num);
+
+            Assert.Equal(expected, stack.Peek());
+        }
+    }
+}
